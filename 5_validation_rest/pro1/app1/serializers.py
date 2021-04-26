c7412bbb73b77e9c600adb7534d34a7799cbb75f
swapnil <swapnil@HP.example.com>
2021-04-26 17:43:07 +0530

    validation in rest

---
+++ 5_validation_rest/pro1/app1/serializers.py
@@ -0,0 +1,48 @@
+from .models import Student
+from rest_framework import serializers
+
+# validators://
+
+
+def start_with_r(value):
+    if value[0].lower() != 'r':
+        raise serializers.ValidationError('name should be start with r')
+
+
+class StudentSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100, validators=[start_with_r])
+    roll = serializers.IntegerField()
+    city = serializers.CharField(max_length=100)
+
+    # field level validation
+
+    def validate_roll(self, value):
+        if value >= 200:
+            raise serializers.ValidationError('seat full')
+        return value
+
+    # object level validation
+
+    def validate(self, data):
+        print(data)
+        nm = data.get('name')
+        ct = data.get('city')
+        print(nm, ct)
+        if nm.lower() == 'swapnil' and ct.lower() != 'nanded':
+            raise serializers.ValidationError("city must be nanded")
+        return data
+
+
+# for adding the data
+
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
+
+# for data update
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.roll = validated_data.get('roll', instance.roll)
+        instance.city = validated_data.get('city', instance.city)
+        instance.save()
+        return instance
